b1723ce6809448eccd302740b548c4b8ce3dabc1
alishakavene <akmunywoki@gmail.com>
2020-08-02 18:15:20 +0300

    add past dates and today's dates

--- pictures/views.py
+++ pictures/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -31,15 +31,23 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_photo(request,past_date):
+# View Function to present news from past days
+def past_days_photo(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
 
+    return render(request, 'photogallery/past-photos.html', {"date": date})
+    
+def photo_of_day(request):
+    date = dt.date.today()
+    return render(request, 'photogallery/today-photos.html', {"date": date,})
