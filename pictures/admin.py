fa0c7e81630c43087c00c84598237bc7e72bf71a
alishakavene <akmunywoki@gmail.com>
2020-08-02 20:02:18 +0300

    create admin file

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Name,Image,tags
 
 # Register your models here.
+admin.site.register(Name)
+admin.site.register(Image)
+admin.site.register(tags)
