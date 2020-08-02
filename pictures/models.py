1482b9e77c50235c949d7cb46d5344a39c902c7a
alishakavene <akmunywoki@gmail.com>
2020-08-02 19:58:42 +0300

    add unittests

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    image_name = models.CharField(max_length =40)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_name(self):
+        self.save()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length =40)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    description = models.TextField()
+    name = models.ForeignKey(Name,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    
+    
--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Name,Image,tags
 
 # Create your tests here.
+class NameTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.food= Name(image_name = 'food')
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Name))
+    
+     # Testing Save Method
+    def test_save_method(self):
+        self.food.save_name()
+        names = Name.objects.all()
+        self.assertTrue(len(names) > 0)
