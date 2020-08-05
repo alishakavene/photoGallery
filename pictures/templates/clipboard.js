e87a9b5153f5c6401ed92f21a51a1a36cf542dc4
alishakavene <akmunywoki@gmail.com>
2020-08-05 11:26:05 +0300

    add media iages and zoom effect

--- pictures/templates/clipboard.js
+++ pictures/templates/clipboard.js
@@ -1,20 +1,7 @@
-function myFunction() {  /* Get the text field */  
-    var copyText = document.getElementById("myInput");
-
-
-      /* Select the text field */  
-    copyText.select(); 
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-      /* Copy the text inside the text field */  
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
     document.execCommand("copy");
-
-
-      /* Alert the copied text */  
-    alert("Copied the text: " + copyText.value);
+    $temp.remove();
 }
-var copyTextarea = document.getElementById("someTextAreaToCopy");
-copyTextarea.select(); //select the text area
-document.execCommand("copy"); //copy to clipboard
-
-document.execCommand("copy");
