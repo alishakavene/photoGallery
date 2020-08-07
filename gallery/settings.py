43535c2726610590f2a5cb71920c2236e741bb23
alishakavene <akmunywoki@gmail.com>
2020-08-07 12:53:37 +0300

    change settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'photogallery-picha.herokuapp.com']
 
 
 # Application definition
