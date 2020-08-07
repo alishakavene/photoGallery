8bdc99b637131541bfcdcac2ec1b468cea41b2ed
alishakavene <akmunywoki@gmail.com>
2020-08-07 13:50:17 +0300

    urls.py

--- pictures/urls.py
+++ pictures/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 
 
 urlpatterns=[
